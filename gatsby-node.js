c8bcd4d3c51d4663040278bdb56a35168c2144ad
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-10-06 15:18:22 +0500

    add redirect to gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,8 +2,13 @@
 const path = require("path")
 
 exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-
+  const { createNodeField, createRedirect } = actions
+  createRedirect({
+    fromPath: '/',
+    toPath: '/en/',
+    isPermanent: true,
+    redirectInBrowser: true,
+  })
   if (node.internal.type === "MarkdownRemark") {
     const fileName = path.basename(node.fileAbsolutePath, ".md") // navbar.en
     const [key, lang] = fileName.split(".") // ["navbar", "en"]
